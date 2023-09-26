fbb2af5c842fb7f1874b167414771c742c5196b6
Sandi625 <sandipermadi625@gmail.com>
2023-09-26 20:41:31 +0700

    second commit

--- js/script.js
+++ js/script.js
@@ -102,3 +102,60 @@ var swiper = new Swiper(".home-slider", {
        
       },
   });
+
+
+  var swiper = new Swiper(".blogs-slider", {
+    // loop:true,
+    grabCursor: true,
+    spaceBetween: 10,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+           
+          },
+        768: {
+          slidesPerView: 2,
+         
+        },
+        991: {
+          slidesPerView: 3,
+          
+        },
+        
+       
+      },
+  });
+
+
+
+  var swiper = new Swiper(".clients-slider", {
+    // loop:true,
+    grabCursor: true,
+    spaceBetween: 20,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+           
+          },
+        640: {
+          slidesPerView: 2,
+         
+        },
+        768: {
+          slidesPerView: 3,
+          
+        },
+        1024: {
+          slidesPerView: 4,
+         
+        },
+      },
+  });
